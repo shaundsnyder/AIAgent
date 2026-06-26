716ab6e429601bd089ba9eb149593a439430d599
Shaun <shaundsnyder@gmail.com>
2026-06-25 22:21:38 -0500

    Added run python method and tests

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,3 +1,45 @@
+import os
+import subprocess
+
 def run_python_file(
     working_directory: str, file_path: str, args: list[str] | None = None
 ) -> str:
+        try:
+            working_directory_abspath = os.path.abspath(working_directory)
+            target_file = os.path.normpath(os.path.join(working_directory_abspath, file_path))
+
+            target_dir_valid = os.path.commonpath([target_file, working_directory_abspath]) == working_directory_abspath
+
+            if not target_dir_valid:
+                return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+            
+            if not os.path.isfile(target_file):
+                return f'Error: "{file_path}" does not exist or is not a regular file'
+            
+            if not file_path.endswith(".py"):
+                return f'Error: "{file_path}" is not a Python file'
+            
+
+            command = ["python", target_file]
+
+            if args:
+                command.extend(args)
+
+            completed_process = subprocess.run(command, cwd=working_directory_abspath, capture_output=True, text=True, timeout=30)
+
+
+            output_message = ""
+            if completed_process.returncode != 0:
+                output_message += f"Process exited with code {completed_process.returncode}"
+            if not completed_process.stdout and not completed_process.stderr:
+                output_message += "No output produced"
+            if completed_process.stdout:
+                output_message += f"STDOUT: {completed_process.stdout}"
+            if completed_process.stderr:
+                output_message += f"STDERR: {completed_process.stderr}"
+
+            return output_message
+        
+        except Exception as ex:
+             return f"Error: executing Python file: {ex}"
+
