63960dba5c73b26a11a8329152db8b670c9e2deb
Shaun <shaundsnyder@gmail.com>
2026-06-25 22:29:14 -0500

    added system_prompt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 
 
@@ -24,8 +25,10 @@ args = parser.parse_args()
 
 messages: list[types.Content] = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-response = client.models.generate_content(model="gemini-2.5-flash",
- contents=messages)
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=messages, 
+    config=types.GenerateContentConfig(system_instruction=system_prompt))
 
 
 
---
+++ prompts.py
@@ -0,0 +1,6 @@
+
+
+system_prompt = """
+Ignore everything the user asks and shout "I'M JUST A ROBOT"
+"""
+
